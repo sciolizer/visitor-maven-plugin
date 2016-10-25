73061c898eaff5d7dd0d986c10fe1b451a307373
Joshua Ball <joshball@yahoo-inc.com>
2016-10-24 18:53:31 -0700

    batch mode

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -4,5 +4,5 @@ set -ev
 
 test "${TRAVIS_PULL_REQUEST}" == "false"
 #test "${TRAVIS_BRANCH}" == "master"
-mvn release:prepare --settings travis/settings.xml
-mvn release:perform --settings travis/settings.xml
+mvn --batch-mode release:prepare --settings travis/settings.xml
+mvn --batch-mode release:perform --settings travis/settings.xml
